7bf28623cf18c2a2bf3632f5c21c9c326dad09f0
tiantiaf <tiantiaf@usc.edu>
2021-09-09 23:31:47 +0000

    ignore old preprocess data scripts

--- preprocess_data/training_data_preprocess.sh
+++ preprocess_data/training_data_preprocess.sh
@@ -2,18 +2,6 @@
 
 # audio feature part
 # three datasets are iemocap, crema-d, and msp-improv dataset
-python3 training_data_preprocess.py --input_spec_size 128 --dataset iemocap \
-                        --feature_type mel_spec --validate 1 --win_len 200  \
-                        --aug emotion --norm znorm --shift 1
-
-python3 training_data_preprocess.py --input_spec_size 128 --dataset crema-d \
-                        --feature_type mel_spec --validate 1 --win_len 200  \
-                        --aug emotion --norm znorm --shift 1
-
-python3 training_data_preprocess.py --input_spec_size 128 --dataset msp-improv \
-                        --feature_type mel_spec --validate 1 --win_len 200  \
-                        --aug emotion --norm znorm --shift 1
-
 
 python3 adversary_data_preprocess.py --input_spec_size 128 --dataset iemocap \
                         --feature_type mel_spec --validate 1 --win_len 200  \
